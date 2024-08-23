afc691fc54a4ff86642038d1005441dda2f26cb0
Carl Thronson <carlthronson@gmail.com>
2024-08-23 15:35:47 -0700

    Add Card controller

---
+++ backend/src/main/java/personal/carlthronson/flashcards/ctrl/CardController.java
@@ -0,0 +1,100 @@
+package personal.carlthronson.flashcards.ctrl;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import personal.carlthronson.flashcards.dto.Card;
+import personal.carlthronson.flashcards.entity.CardEntity;
+import personal.carlthronson.flashcards.svc.CardService;
+
+@RestController
+@EnableWebMvc
+@Transactional
+public class CardController {
+
+  @Autowired
+  CardService service;
+  Logger logger = Logger.getLogger(CardController.class.getName());
+  ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
+
+  @RequestMapping(path = "/card/{id}", method = RequestMethod.DELETE)
+  public void delete(@PathVariable(name = "id") Long id) {
+    service.delete(id);
+  }
+
+  @RequestMapping(path = "/card", method = RequestMethod.POST)
+  public CardEntity save(@RequestBody Card requestBody) {
+    logger.info("Request body: " + requestBody);
+    CardEntity cardEntity1 = new CardEntity();
+    cardEntity1.setId(requestBody.getId());
+    cardEntity1.setName(requestBody.getName());
+    cardEntity1.setLabel(requestBody.getLabel());
+    CardEntity cardEntity = cardEntity1;
+    cardEntity = service.save(cardEntity);
+    return cardEntity;
+  }
+
+  @RequestMapping(path = "/card/getbyid/{id}", method = RequestMethod.GET)
+  public Card getById(@PathVariable("id") Long id) {
+    logger.info("Path variable: " + id);
+    CardEntity cardEntity = service.getById(id);
+    Card card = new Card(cardEntity);
+    return card;
+  }
+
+  @RequestMapping(path = "/card/findbyid/{id}", method = RequestMethod.GET)
+  public CardEntity findById(@PathVariable("id") Long id) {
+    logger.info("Path variable: " + id);
+    return service.findById(id);
+  }
+
+  @RequestMapping(path = "/card/findallbyid/{id}", method = RequestMethod.GET)
+  public List<CardEntity> findAllById(@PathVariable("id") Long id) {
+    logger.info("Path variable: " + id);
+    return service.findAllById(id);
+  }
+
+  @RequestMapping(path = "/card/findbyname/{name}", method = RequestMethod.GET)
+  public CardEntity findByName(@PathVariable("name") String name) {
+    logger.info("Path variable: " + name);
+    return service.findByName(name);
+  }
+
+  @RequestMapping(path = "/card/findallbyname/{name}", method = RequestMethod.GET)
+  public List<CardEntity> findAllByName(@PathVariable("name") String name) {
+    logger.info("Path variable: " + name);
+    return service.findAllByName(name);
+  }
+
+  @RequestMapping(path = "/card/findbylabel/{label}", method = RequestMethod.GET)
+  public CardEntity findByLabel(@PathVariable("label") String label) {
+    logger.info("Path variable: " + label);
+    return service.findByLabel(label);
+  }
+
+  @RequestMapping(path = "/card/findallbylabel/{label}", method = RequestMethod.GET)
+  public List<CardEntity> findAllByLabel(@PathVariable("label") String label) {
+    logger.info("Path variable: " + label);
+    return service.findAllByLabel(label);
+  }
+
+  @RequestMapping(path = "/card/findall", method = RequestMethod.GET)
+  public List<CardEntity> findAll(@RequestParam("limit") Optional<Integer> limit, Principal principal) {
+    logger.info("Request param limit: " + limit);
+    return service.findAll(limit).getContent();
+  }
+}
---
+++ backend/src/main/java/personal/carlthronson/flashcards/dto/Card.java
@@ -0,0 +1,37 @@
+package personal.carlthronson.flashcards.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import personal.carlthronson.flashcards.entity.CardEntity;
+
+public class Card {
+
+  public Card() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public Card(CardEntity entity) {
+    if (entity != null) {
+      this.setId(entity.getId());
+      this.setName(entity.getName());
+      this.setLabel(entity.getLabel());
+      this.setQuestion(entity.getQuestion());
+    }
+  }
+
+  @Getter
+  @Setter
+  private Long id;
+
+  @Getter
+  @Setter
+  String name;
+
+  @Getter
+  @Setter
+  private String label;
+
+  @Getter
+  @Setter
+  private String question;
+}
